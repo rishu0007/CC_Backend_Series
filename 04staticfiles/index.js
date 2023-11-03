e056c4a0bfad6adfaa384177b1c376002dc5df9d
Rishu <kumarrishu571@gmail.com>
2023-11-03 19:34:41 +0530

    static files and error handling

---
+++ 04staticfiles/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+app.set("view engine", "ejs");
+app.use(express.static('./public'));
+
+app.get('/', function (req, res) {
+  res.render("index")
+})
+
+app.get("/error", function(req, res, next) {
+    throw Error("Something went wrong");
+})
+
+// app.get('/contact', function(req, res) {
+//     res.render("contact")
+// })
+
+app.use(function errorHandler (err, req, res, next) {
+    if (res.headersSent) {
+        return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
+})
+
+app.listen(3000)
