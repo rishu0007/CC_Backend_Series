96c7887c3afa52b78dcd2a9f7ce67788e0357605
Rishu <kumarrishu571@gmail.com>
2023-11-06 21:52:06 +0530

    mongodb sessin cookie

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users");
 
-/* GET home page. */
-router.get('/', function(req, res) {
+// router.get('/', function(req, res) {
+//   res.render("index");
+// });
+
+// router.get('/create', async function(req, res) {
+//   const createduser = await userModel.create({
+//     username: "Rishu",
+//     age: 22,
+//     name: "Rishu"
+//   });
+//   res.send(createduser);
+// });
+
+router.get("/", function(req, res) {
+  req.session.ban = true;
+  res.cookie("age", 25);
   res.render("index");
+})
+
+router.get("/read", function(req,res) {
+  console.log(req.cookies.age);
+  res.send("check");
+});
+
+
+router.get("/delete", function(req,res) {
+  res.clearCookie("age");
+  res.send("clear hogai");
+});
+
+router.get("/checkban", function(req, res) {
+  console.log(req.session);
+  res.send("check kiya h console dekho");
 });
 
 module.exports = router;
--- demo/routes/users.js
+++ demo/routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+// const mongoose = require("mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// mongoose.connect("mongodb://127.0.0.1:27017/practicekaro");
 
-module.exports = router;
+// const userschema = mongoose.Schema({
+//   username: String,
+//   name: String,
+//   age: Number
+// })
+
+// module.exports = mongoose.model("user", userschema);
+
