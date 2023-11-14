a76cc3b51ebf41d06a7c219886d131db24a47df5
Rishu <kumarrishu571@gmail.com>
2023-11-14 18:23:28 +0530

    flash message and mongodb

--- flashMsg/routes/index.js
+++ flashMsg/routes/index.js
@@ -1,20 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const userModel = require("./users");
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index');
 });
 
-router.get('/failed', function(req, res) {
-  req.flash("age", 22);
-  req.flash("name", "Rishu");
-  res.send("bangya");
-});
+// router.get('/failed', function(req, res) {
+//   req.flash("age", 22);
+//   req.flash("name", "Rishu");
+//   res.send("bangya");
+// });
 
-router.get('/check', function(req, res) {
-  console.log(req.flash("age"), req.flash("name"));
-  res.send("check backend terminal");
+// router.get('/check', function(req, res) {
+//   console.log(req.flash("age"), req.flash("name"));
+//   res.send("check backend terminal");
+// });
+
+router.get("/create", async function(req,res) {
+  let userdata = await userModel.create({
+    username: "Rishu",
+    nickname: "Chandan",
+    description: "I am 22 and I love Cricket",
+    categories: ['js', 'node', 'react', 'gsap', 'modern animations']
+  })
+  res.send(userdata);
 });
 
 module.exports = router;
--- flashMsg/routes/users.js
+++ flashMsg/routes/users.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// module.exports = router;
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/testingendgame")
+
+const userSchema = mongoose.Schema({
+  username: String,
+  nickname: String,
+  description: String,
+  categories: {
+    type: Array,
+    default: []
+  },
+  datecreated: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
-module.exports = router;
+module.exports = mongoose.model("user", userSchema);
